7bbbb6c8872301b08aa482d4bb5fb50fc1096a1a
Anushree Bagchi <anushreebagchi4791@gmail.com>
2020-01-07 21:06:38 +0530

    Iplementing queue with linked list

---
+++ implement_queue_linkedList.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.queue_size = 0
+    
+    def size(self):
+        return self.queue_size
+    def is_empty(self):
+        return self.queue_size == 0
+    def enqueue(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(value)
+            self.tail = self.tail.next
+        self.queue_size += 1
+    def dequeue(self):
+        if self.head is None:
+            return None
+        value = self.head.value
+        self.head = self.head.next
+        self.queue_size -=1
+        return value
+    def top(self):
+        return self.head.value
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print ("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print ("Pass" if (q.dequeue() == 2) else "Fail")
+print ("Pass" if (q.dequeue() == 3) else "Fail")
+print ("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print ("Pass" if (q.size() == 1) else "Fail")
+
